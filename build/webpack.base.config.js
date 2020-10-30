7f436c88c2b90bbe17b0cb9d6065db41839dbe1f
shuo.liu <shuo.liu@zhangmen.com>
2020-10-30 18:02:58 +0800

    babel-loader

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -19,6 +19,9 @@ module.exports = {
                 test: /\.(js|jsx)$/,//一个匹配loaders所处理的文件的拓展名的正则表达式，这里用来匹配js和jsx文件（必须）
                 exclude: /node_modules/,//屏蔽不需要处理的文件（文件夹）（可选）
                 loader: 'babel-loader',//loader的名称（必须）
+                options:{
+                    cacheDirectory: true
+                }
             },
             {
                 test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
